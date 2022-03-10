c8de29916bf290c4050f63118cf0f2794ebb8d3e
chrisais9 <chrisais9@naver.com>
2022-03-10 13:01:45 +0900

    :white_check_mark: 생성자 관련 테스트 코드 추가

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,48 @@
+/* eslint-disable node/no-missing-import */
+/* eslint-disable no-unused-vars */
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { HoneyXBadger } from "../typechain";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("HoneyXBadger", function () {
+  let honeyXBadger: HoneyXBadger;
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
+  let addrs: SignerWithAddress[];
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  beforeEach(async function () {
+    const HoneyXBadgerContract = await ethers.getContractFactory(
+      "HoneyXBadger"
+    );
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    honeyXBadger = await HoneyXBadgerContract.deploy(
+      "HoneyXBadger",
+      "HXB",
+      10000
+    );
+    await honeyXBadger.deployed();
+  });
+  describe("Deployment", function () {
+    it("Should return right name", async function () {
+      expect(await honeyXBadger.name()).to.equal("HoneyXBadger");
+    });
+    it("Should return right symbol", async function () {
+      expect(await honeyXBadger.symbol()).to.equal("HXB");
+    });
+    it("Should return right max supply", async function () {
+      expect(await honeyXBadger.maxSupply()).to.equal(10000);
+    });
+    // it("Should return right name", async function () {
+    //   const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    //   // wait until the transaction is mined
+    //   await setGreetingTx.wait();
+
+    //   expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // });
   });
 });
