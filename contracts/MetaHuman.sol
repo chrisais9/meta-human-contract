36be7f3ffdbbf76cbf37c109ca4ea84e2a0430fd
chrisais9 <chrisais9@naver.com>
2022-06-09 16:43:57 +0900

    :rotating_light: pin solidity compiler to 0.8.11 which is klaytn-compatible

--- contracts/MetaHuman.sol
+++ contracts/MetaHuman.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.11;
 pragma abicoder v2;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
